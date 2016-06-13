c891ee3abb1411134de0bc10ad6d7bfcce35f157
Marjorie Salvetat <marjorie.salvetat@gmail.com>
2016-06-13 12:57:40 +0200

    Initialization of the Maven project

---
+++ src/main/java/fr/irit/smac/demoamasfactory/App.java
@@ -0,0 +1,13 @@
+package fr.irit.smac.demoamasfactory;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
